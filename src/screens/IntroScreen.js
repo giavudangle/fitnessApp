bbd079befffadf77fefbe19a757b4a0efb641645
Vudang <vudang@Vudangs-iMac.local>
2020-08-05 10:59:26 +0700

    Link Library and Create Mock Intro Screen

--- src/screens/IntroScreen.js
+++ src/screens/IntroScreen.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
-import {Text,View} from 'react-native';
+import {View,StyleSheet,ActivityIndicator,TouchableOpacity} from 'react-native';
+
+import {Text,Icon,Image,Button} from 'react-native-elements';
+
 
 export default class IntroScreen extends Component{
   render(){
     return(
-      <View>
-        <Text>IntroScreen</Text>
+      <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Icon name="md-fitness" size={80} type="ionicon" />
+        <Text h4>Welcome to Fitness </Text>
+        <Text h5 style={{textAlign: 'center'}}>
+          The best App for track Fitness workout and Meal planner
+        </Text>
       </View>
+    </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F4F6FA',height:'100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  headerContainer: {
+    top: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 40
+  },
+});
